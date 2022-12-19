fbaaa73ecee25fa7435f76cc1976a8471090162b
Eduardo <eduardosmatos_@hotmail.com>
2022-12-19 18:50:29 -0300

    ex008

--- ex007/script.js
+++ ex007/script.js
@@ -1,10 +1,11 @@
-var res = document.getElementById('res')
-function média() {
-    var nome = window.prompt('Qual é o nome do aluno?')
-    let nota1 = window.prompt(`Qual foi a primeira nota de ${nome}?`)
-    let nota2 = window.prompt(`Além de ${nota1}, qual foi a outra nota de ${nome}?`)
-    let média = (Number(num1) + Number(num2)) / 2
-    res.innerHTML = `Calculando a média final de <mark>${nome}</mark>.`
-    res.innerHTML = `As notas obtidas foram <mark>${nota1}</mark> e <mark>${nota2}</mark>.`
-    res.innerHTML = `A média final será <mark>${média}</mark>.`
+function media() {
+    let nome = window.prompt('Qual é o nome do aluno?')
+    let nota1 = Number(window.prompt(`Qual foi a primeira nota de ${nome}?`))
+    let nota2 = Number(window.prompt(`Além de ${nota1}, qual foi a outra nota de ${nome}?`))
+    med = (nota1 + nota2)/2
+
+    let res = document.getElementById('res')
+    res.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark>.</p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${nota1}</mark> e <mark>${nota2}</mark>.</p>`
+    res.innerHTML += `<p>A média final será <mark>${med}</mark>.</p>`
 }
