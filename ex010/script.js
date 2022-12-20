2fb19c5869840c58f0d88257c0c4d3921a22d5c3
Eduardo <eduardosmatos_@hotmail.com>
2022-12-20 18:20:19 -0300

    ex010

---
+++ ex010/script.js
@@ -0,0 +1,17 @@
+res = document.getElementById('res')
+
+function primeiro() {
+    res.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+
+function segundo() {
+    res.innerHTML += `<p>Clicou no segundo botão</p>`
+}
+
+function terceiro() {
+    res.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+
+function quarto() {
+    res.innerHTML += `<p>Clicou no quarto botão</p>`
+}
